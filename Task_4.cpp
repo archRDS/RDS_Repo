1265ab4c071e4e5890bffd057ea0dd0bd68729c7
archRDS <2007dima1611@gmail.com>
2026-05-06 23:13:39 +0400

    Update Task_4.cpp

--- Task_4.cpp
+++ Task_4.cpp
@@ -4,50 +4,49 @@
 using namespace std;
 
 int simulateGame(int n, int m, const vector<int>& arr) {
-    int pos = 0, pavelScore = 0, vikaScore = 0;
-    int lastMovePavel = 0, lastMoveVika = 0;
-    bool pavelTurn = true;
+    int pos = 0, pavelScore = 0, vikaScore = 0;
+    int pavelLastMove = 0;  // Сколько Павел взял в СВОЙ предыдущий ход
+    int vikaLastMove = 0;   // Сколько Вика взяла в СВОЙ предыдущий ход
+    bool pavelTurn = true;
 
-    while (pos < n) {
-        int remaining = n - pos;
-        int maxTake = min(m, remaining);
-        int bestK = 1, maxSum = -2e9;
+    while (pos < n) {
+        int remaining = n - pos;
+        int maxTake = min(m, remaining);
+        int bestK = 1, maxSum = -2e9;
 
-        for (int k = 1; k <= maxTake; ++k) {
-            // Вариант 2: нельзя брать столько же, сколько взял противник
-            if (!pavelTurn && k == lastMovePavel) continue;
-            if (pavelTurn && k == lastMoveVika) continue; // Для честности симметрии
+        for (int k = 1; k <= maxTake; ++k) {
+            // ВАРИАНТ 1: нельзя брать столько же, сколько взял САМ в свой предыдущий ход
+            if (pavelTurn && k == pavelLastMove) continue;  // Павел не может повторить свой ход
+            if (!pavelTurn && k == vikaLastMove) continue;  // Вика не может повторить свой ход
 
-            int currentSum = 0;
-            for (int i = pos; i < pos + k; ++i) currentSum += arr[i];
+            int currentSum = 0;
+            for (int i = pos; i < pos + k; ++i) currentSum += arr[i];
 
-            if (currentSum > maxSum || (currentSum == maxSum && k < bestK)) {
-                maxSum = currentSum;
-                bestK = k;
-            }
-        }
+            if (currentSum > maxSum || (currentSum == maxSum && k < bestK)) {
+                maxSum = currentSum;
+                bestK = k;
+            }
+        }
 
-        if (pavelTurn) {
-            pavelScore += maxSum;
-            lastMovePavel = bestK;
-        } else {
-            vikaScore += maxSum;
-            lastMoveVika = bestK;
-        }
-        pos += bestK;
-        pavelTurn = !pavelTurn;
-    }
-    return (pavelScore > vikaScore) ? 1 : 0;
+        if (pavelTurn) {
+            pavelScore += maxSum;
+            pavelLastMove = bestK;  // Запоминаем ход Павла
+        } else {
+            vikaScore += maxSum;
+            vikaLastMove = bestK;   // Запоминаем ход Вики
+        }
+        pos += bestK;
+        pavelTurn = !pavelTurn;
+    }
+    return (pavelScore > vikaScore) ? 1 : 0;
 }
-
 int main() {
-    int n, m;
-    cout << "Введите n и m: ";
-    cin >> n >> m;
-    vector<int> arr(n);
-    cout << "Введите последовательность: ";
-    for (int &x : arr) cin >> x;
-
-    cout << simulateGame(n, m, arr) << endl;
-    return 0;
+    int n, m;
+    cout << "Введите n и m: ";
+    cin >> n >> m;
+    vector<int> arr(n);
+    cout << "Введите последовательность: ";
+    for (int &x : arr) cin >> x;
+    cout << simulateGame(n, m, arr) << endl;
+    return 0; 
 }
